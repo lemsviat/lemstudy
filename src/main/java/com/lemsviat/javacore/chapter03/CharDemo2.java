2844f19b2d02b7d4091258c6fbbe63393185d543
Sviatoslav Lementar <lemsviat@gmail.com>
2020-03-18 17:41:14 +0200

    3-4

---
+++ src/main/java/com/lemsviat/javacore/chapter03/CharDemo2.java
@@ -0,0 +1,11 @@
+package main.java.com.lemsviat.javacore.chapter03;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1='X';
+        System.out.println("ch1 содержит " + ch1);
+        ch1++;
+        System.out.println("ch1 теперь содержит " + ch1);
+    }
+}
