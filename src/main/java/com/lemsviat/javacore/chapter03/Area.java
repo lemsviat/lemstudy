990cbdead32d14569ac4ce5a38c558f1c410e088
Sviatoslav Lementar <lemsviat@gmail.com>
2020-03-18 14:44:45 +0200

    3-2

---
+++ src/main/java/com/lemsviat/javacore/chapter03/Area.java
@@ -0,0 +1,11 @@
+package main.java.com.lemsviat.javacore.chapter03;
+
+public class Area {
+    public static void main(String[] args) {
+        double  r, a;
+        r=10.8;
+        a=Math.PI*Math.pow(r,2);
+        System.out.println("Площадь круга равна "+a);
+
+    }
+}
