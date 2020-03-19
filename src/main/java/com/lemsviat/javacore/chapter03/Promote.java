371717ef0a4481dbc10664c7fb645d2d6509a745
Sviatoslav Lementar <lemsviat@gmail.com>
2020-03-19 12:06:31 +0200

    3-11

---
+++ src/main/java/com/lemsviat/javacore/chapter03/Promote.java
@@ -0,0 +1,15 @@
+package main.java.com.lemsviat.javacore.chapter03;
+
+public class Promote {
+    public static void main(String[] args) {
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 5000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f * b) + (i / c) - (d * s);
+        System.out.println((f * b) + " + " + (i / c) + " - " + (d * s));
+        System.out.println("result = " + result);
+    }
+}
